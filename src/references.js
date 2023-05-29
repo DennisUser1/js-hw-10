2db2d9ac08c1e37fc8a42b03aa6451d01abfa630
DennisUser1 <dennis3ace@gmail.com>
2023-05-29 17:14:39 +0300

    fix bug + replacing src folder

--- src/references.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-  input: document.getElementById('search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
-};
-
-export default refs
